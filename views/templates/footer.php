f8c310e73e9962374d027112427abe70bada9f87
Marko Buhovac <marko.buhovac@ext.ec.europa.eu>
2026-02-06 09:39:52 +0100

    Footer adjustment.

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -10,9 +10,20 @@
       <li><a href="#">Conditions d'Utilisation</a></li>
     </ul>
   </nav>
-  <p>&copy; <span id="current-year">2025</span> Nom de l'Application. Tous droits réservés.</p>
+  <p>&copy; <span id="current-year">2026</span> Projet Web — Marko Buhovac. Tous droits réservés.
+  </p>
   <script>document.getElementById('current-year').textContent = new Date().getFullYear();</script>
 </footer>
+<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<script>
+    AOS.init({
+        duration: 900,
+        easing: "ease-out",
+        offset: 120,
+        delay: 0,
+        once: true,
+    });
+</script>
 <script defer src="scripts/app.js"></script>
 </body>
 </html>
